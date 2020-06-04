68ceb212206de8b468744b9fa6b7934a45c09f85
Miko <13miko.s@gmail.com>
2020-06-03 20:59:35 -0500

    get rid of print

--- hashmap/node.py
+++ hashmap/node.py
@@ -6,8 +6,14 @@ class Node:
         self.prev = None
 
     def remove(self):
+        if self.prev is not None and self.next is not None:
+            self.prev.next, self.next.prev = self.next, self.prev
+            self.prev, self.next = None, None
         if self.prev is not None:
-            self.prev.next = self.next
+            self.prev.next = None
         if self.next is not None:
-            self.next.prev = self.prev
-        self.next, self.prev = None, None
+            self.next.prev = None
+        return
+
+    def __repr__(self):
+        return f"({self.key}, {self.value})"
